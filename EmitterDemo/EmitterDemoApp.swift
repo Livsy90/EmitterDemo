153e5c66ad001a7ed8ea8bed1b6ead4b7d57e280
Livsy90 <artmir@live.ru>
2024-09-02 13:49:41 +0400

    Initial Commit

---
+++ EmitterDemo/EmitterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmitterDemoApp.swift
+//  EmitterDemo
+//
+//  Created by Livsy on 02.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmitterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
